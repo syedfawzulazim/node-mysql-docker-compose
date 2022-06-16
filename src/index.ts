8cab67e0c6c0ef05e78ff3b6f205d2c9c8e62bcc
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2022-06-16 14:07:06 +0200

    mysql server with docker-compose file

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.get("/", (req,res) => {
-    res.send("Hello World1!!!")
+    res.send("Hello World1!!!!!!")
 })
 
 app.listen(PORT, () => console.log(`Server Running at ${PORT}`))
