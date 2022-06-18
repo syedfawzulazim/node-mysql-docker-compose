8e9aeac9d2fb263a7787398bf6d396cec8e950ca
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2022-06-18 17:10:48 +0200

    modified

---
+++ src/models/user.ts
@@ -0,0 +1,41 @@
+import { DataTypes, Model, Sequelize } from 'sequelize'
+import sequelize from '../database/connection'
+
+class User extends Model {
+  public id!: number
+  public name!: string
+  public subject!: string
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    subject: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    createdAt: {
+      type: new DataTypes.DATE(),
+      defaultValue: Sequelize.literal('NOW()'),
+      allowNull: false,
+    },
+    updatedAt: {
+      type: new DataTypes.DATE(),
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 'users',
+    sequelize,
+  },
+)
+
+export default User
