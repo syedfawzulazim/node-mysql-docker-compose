11386434be7b5a0d4448aefc926d17f049a69cd7
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2022-06-24 11:53:02 +0200

    updated

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import sequelize from './src/database/connection'
+import User from './src/calculators/machinen/models/user'
+
+const PORT = process.env.PORT || 8080
 
 const app = express()
-
-const PORT = process.env.PORT || 8000
+app.use(express.json())
 
 
 const checkConnection = async () => {
@@ -14,10 +16,32 @@ const checkConnection = async () => {
         console.error('Unable to connect to the database:', error);
     }
 }
-checkConnection();
+//checkConnection();
 
-app.get("/", (req,res) => {
+app.get("/", async (req,res) => {   
     res.send("Hello World1!!!!!!")
+})
+
+app.get("/get-user", async (req,res) => {
+
+    // const users = await User.findAll({where: {
+    //     age: 23
+    // }})
+    const users = await User.findAll({
+        attributes: ['name', 'age']
+    })
+
+
+    res.send({users: users})
+})
+
+app.post('/add-user', async (req, res) => {
+    const request = req.body;
+    
+    const newUser = User.build({ name: request.name, age: request.age})
+    await newUser.save();
+
+    res.send({message: 'User Added Successfully', res: request})
 })
 
 app.listen(PORT, () => console.log(`Server Running at ${PORT}`))
